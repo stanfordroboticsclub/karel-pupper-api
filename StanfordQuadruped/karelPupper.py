1ac324497987f3d082088e0d1ee6a4e4076eaa3a
branvu <brangogohi@gmail.com>
2021-11-26 00:48:54 -0800

    Implemented smooth wakeup and rest and move until blocked

--- StanfordQuadruped/karelPupper.py
+++ StanfordQuadruped/karelPupper.py
@@ -22,11 +22,14 @@ import imutils
 
 DIRECTORY = "logs/"
 FILE_DESCRIPTOR = "walking"
+BLURRY_THRESHOLD = 3
+
 class BehaviorState(Enum):
     DEACTIVATED = -1
     REST = 0
     TROT = 1
     WALK = 2
+# TODO: figure out how to know if you are left standing or resting
 class Pupper:      
 
     # if __name__ == "__main__":
@@ -36,17 +39,17 @@ class Pupper:
     #     parser.add_argument("--home", help="home the motors (moves the legs)", action="store_true")
     #     FLAGS = parser.parse_args()
     #     main(FLAGS)
-    def __init__(self, args=None):
+    def __init__(self, args=None, LOG=False):
         # Eventually have arguments passed in
         # Create config
         self.config = Configuration()
         self.hardware_interface = HardwareInterface.HardwareInterface(port=SERIAL_PORT)
         time.sleep(0.1)
         self.controller = Controller(self.config, four_legs_inverse_kinematics)
-        self.state = State(height=self.config.default_z_ref)
+        self.state = State(height= -0.05) # self.config.default_z_ref
         self.input_curve = lambda x: np.sign(x) * min(x ** 2, 1)
         self.vs = VideoStream(usePiCamera=0).start()
-    
+        self.LOG = LOG
     def wakeup(self):
         """Main program"""
 
@@ -88,13 +91,17 @@ class Pupper:
         # print("Waiting for L1 to activate robot.")
 
         last_loop = time.time()
-        print("Wokeup.")
-        time.sleep(0.1)
+        if self.LOG:
+            print("Wokeup.")
+        
         self.hardware_interface.serial_handle.reset_input_buffer()
         time.sleep(0.1)
+        
         self.hardware_interface.activate()
-        time.sleep(0.1)
+        time.sleep(1)
         self.state.activation = 1
+        self.slowStand()
+        time.sleep(1)
         command = Command(self.config.default_z_ref)
         self.controller.run(self.state, command)
         self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
@@ -137,19 +144,66 @@ class Pupper:
         #         print("Closing log file")
         #         log_file.close()
     '''
-    The Pupper rests by returning to its sleeping position.
+    only slow stand from bottom position (clarify what the default position is)
+    '''
+    def slowStand(self):
+        print(self.state.height)
+        cur = self.state.height # 0
+        target = self.config.default_z_ref
+        startTime = time.time()
+        last_loop = startTime
+        command = Command(-0.03)
+        while abs(cur) < abs(target):
+            if time.time() - last_loop >= self.config.dt:
+                command.height = ((time.time() - startTime) / 2) * target - 0.03
+                cur = command.height
+                self.controller.run(self.state, command)
+                self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
+                last_loop = time.time()
+    def is_blocked(self):
+        img = cv2.cvtColor(self.vs.read(), cv2.COLOR_BGR2GRAY)
+        lap = cv2.Laplacian(img, cv2.CV_16S)
+        mean, stddev = cv2.meanStdDev(lap)
+        #print(stddev[0, 0])
+        if stddev[0,0] < BLURRY_THRESHOLD:
+            return True
+        return False
+    '''
+    The Pupper rests by returning to its sleeping position. It deactivates at the end.
     '''
     def rest(self):
-        print("Resting")
+        if self.LOG:
+            print("PUPPER IS RESTING")
+        self.nap()
         time.sleep(0.1)
         self.hardware_interface.deactivate()
         time.sleep(0.1)
         self.state.activation = 0
 
     '''
-    The Pupper turns left with positive angle. Count deltas until 90
+    The Pupper naps but does not deactivate
+    '''
+    def nap(self):
+        cur = self.state.height
+        orig = cur
+        target = self.config.default_z_ref
+        startTime = time.time()
+        last_loop = startTime
+        command = Command(self.config.default_z_ref)
+        while cur <= -0.03: # equal here?
+            if time.time() - last_loop >= self.config.dt:
+                command.height = orig + ((time.time() - startTime) / 2) * abs(target)
+                cur = command.height
+                self.controller.run(self.state, command)
+                self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
+                last_loop = time.time()
+    '''
+    The Pupper turns left with positive angle. Angle is in radians and speed is in
+    radians / s.
     '''
     def turn(self, angle, speed, behavior=BehaviorState.TROT):
+        if self.LOG:
+            print("TURNING AT", angle, " RADIANS at", speed, " rad/s")
         speed = np.clip(speed, -self.config.max_yaw_rate, self.config.max_yaw_rate)
         target_time = abs(angle) / abs(speed) 
         speed = np.sign(angle) * speed
@@ -191,8 +245,9 @@ class Pupper:
     '''
     def forward_for_time(self, duration, speed, behavior=BehaviorState.TROT):
         command = Command(self.config.default_z_ref)
-        speed = max(min(1, speed), -1)
-        x_vel = self.input_curve(speed) * self.config.max_x_velocity
+        speed = np.clip(speed, -self.config.max_x_velocity, self.config.max_x_velocity)
+
+        x_vel = speed
         y_vel = 0
         command.horizontal_velocity = np.array([x_vel, y_vel])
         command.yaw_rate = 0
@@ -215,9 +270,54 @@ class Pupper:
         self.controller.run(self.state, command)
         self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
     
+    def move_until_blocked(self, speed, behavior=BehaviorState.TROT):
+        command = Command(self.config.default_z_ref)
+        speed = np.clip(speed, -self.config.max_x_velocity, self.config.max_x_velocity)
+        x_vel = speed
+        y_vel = 0
+        command.horizontal_velocity = np.array([x_vel, y_vel])
+        command.yaw_rate = 0
+        command.trot_event = True
+        if behavior == BehaviorState.WALK:
+            command.walk_event = True
+        elif behavior != BehaviorState.TROT:
+            print("Can't rest while moving forward")
+            return
+        startTime = time.time()
+        last_loop = startTime
+        blocked = False
+        while not blocked:
+            # if time.time() - last_loop >= self.config.dt - 0.001:
+            self.controller.run(self.state, command)
+            self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
+            last_loop = time.time()
+            blocked = self.is_blocked()
+            # print(last_loop - time.time())
+        command = Command(self.config.default_z_ref)
+        command.stand_event = True
+
+        self.controller.run(self.state, command)
+        self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
+    
     def getImage(self):
         return cv2.flip(imutils.rotate(self.vs.read(), 180), 1)
-        
+    
+    
+
+    def slowRest(self):
+        cur = self.config.default_z_ref
+        orig = cur
+        target = self.config.default_z_ref
+        startTime = time.time()
+        last_loop = startTime
+        command = Command(self.config.default_z_ref)
+        while cur <= -0.03:
+            if time.time() - last_loop >= self.config.dt:
+                command.height = orig + ((time.time() - startTime) / 2) * abs(target)
+                cur = command.height
+                self.controller.run(self.state, command)
+                self.hardware_interface.set_cartesian_positions(self.state.final_foot_locations)
+                last_loop = time.time()
     '''
     roll - walking at angle
     height - walking shorter
